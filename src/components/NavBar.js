f3682b85543208bdf3f7ccf0662acb543958f217
Daniel Guzman <guzdan@live.com>
2022-10-20 13:51:09 -0500

    commit tree 38a8854214d357efbb3b6d56bf720ea2c2a058e9

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,15 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linksTags = links.map((link) => {
+    return <a key={link} href={`#${link}`}>{link}</a>
+  })
+
+  return (
+  <nav>
+    {linksTags}
+  </nav>
+  )
 }
 
 export default NavBar;
